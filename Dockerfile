49d9541da28fd997fd5eb89798ba4b1a896d1e25
Kir Ushakov <imonare@gmail.com>
2024-07-03 15:13:00 +0300

    Dockerize

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,24 @@
 FROM openjdk:17-jdk-slim
 WORKDIR /app
+
 COPY gradlew .
-COPY build.gradle.kts .
+COPY gradlew.bat .
+RUN chmod +x ./gradlew
+
 COPY gradle ./gradle
+COPY build.gradle.kts .
+COPY settings.gradle.kts .
 COPY src ./src
-RUN chmod +x gradlew
-RUN ./gradlew build
-COPY build/libs/OtusHub.jar /app/OtusHub.jar
-ENV SPRING_DATASOURCE_URL=
-ENV SPRING_DATASOURCE_USERNAME=
-ENV SPRING_DATASOURCE_PASSWORD=
-ENV jwt.secret=
-ENV SERVICE_URL=
+
+RUN ./gradlew clean build
+RUN cp $(find /app/build -name "OtusHub.jar" | head -n 1) OtusHub.jar
+
+ENV SPRING_DATASOURCE_URL=jdbc:postgresql://postgres:5432/otushub
+ENV SPRING_DATASOURCE_USERNAME=postgres
+ENV SPRING_DATASOURCE_PASSWORD=postgres
+ENV JWT_SECRET=eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJLaXIgVXNoYWtvdnYiLCJVc2VybmFtZSI6InVzZXItc2VydmljZSIsImV4cCI6MTcxOTI5ODk0NCwiaWF0IjoxNzE5Mjk4OTQ0fQ.kUPsL0suGFf_7zDyfv1X3kY4ps5UT_m7-UiXjFrmOMA
+ENV SERVICE_URL=http://localhost:4242
 ENV JAVA_OPTS=""
+
 CMD ["sh", "-c", "java $JAVA_OPTS -jar OtusHub.jar"]
 EXPOSE 4242
--- src/main/kotlin/ru/ushakov/otushub/security/config/SecurityConfig.kt
+++ src/main/kotlin/ru/ushakov/otushub/security/config/SecurityConfig.kt
@@ -4,13 +4,18 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.ProviderManager
+import org.springframework.security.config.Customizer.withDefaults
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
 import ru.ushakov.otushub.security.jwt.CustomAuthenticationProvider
 import ru.ushakov.otushub.security.jwt.JwtAuthenticationFilter
 import ru.ushakov.otushub.user.repository.UserRepository
+
 
 @Configuration
 class SecurityConfig {
@@ -34,8 +39,21 @@ class SecurityConfig {
                 ).permitAll()
                     .anyRequest().authenticated()
             }
+            .cors(withDefaults())
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
         return http.build()
+    }
+
+    @Bean
+    fun corsFilter(): CorsFilter? {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        config.allowCredentials = true
+        config.addAllowedOrigin("http://localhost:3000")
+        config.addAllowedHeader("*")
+        config.addAllowedMethod("*")
+        source.registerCorsConfiguration("/**", config)
+        return CorsFilter(source)
     }
 
     @Bean
